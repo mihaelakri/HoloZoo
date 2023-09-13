92c5d0dbd5513b9e63e60ef5136cd23837bd6b87
Ivan Zelentrović <bluez.du@gmail.com>
2023-09-13 21:12:48 +0200

    switched server url to remote

--- Assets/Scripts/CommConstants.cs
+++ Assets/Scripts/CommConstants.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public static class CommConstants
 {
-    public const string ServerURL = "http://localhost:8000/";
+    // public const string ServerURL = "http://localhost:8000/";
+    public const string ServerURL = "http://izelentrovic.com/";
     public static string Auth = "";
     public static string XSRF = "";
+
+    public static string IdUser = "";
 }
