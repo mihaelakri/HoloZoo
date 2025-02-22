48467b41f145fae3577f0100efbf7cd869689c9b
Mihaela <mihaelakri@hotmail.com>
2025-02-22 20:14:31 +0100

    refactor: add close button to pop up wondowv for globe

---
+++ Assets/CloseOblacic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOblacic : MonoBehaviour
+{
+    public void CloseOblacicPanel()
+    {
+        Destroy(gameObject);
+    }
+}
