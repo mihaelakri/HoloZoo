4746529c167d4b1bb55ea606e29e143257a76e50
Ivan Zelentrović <bluez.du@gmail.com>
2025-03-09 22:55:41 +0100

    feat: add back button for device selection

--- Assets/Scripts/ChooseDevice.cs
+++ Assets/Scripts/ChooseDevice.cs
@@ -4,23 +4,33 @@ using UnityEngine.UI;
 public class ChooseDevice : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject backButton;
     public Toggle m_Toggle;
 
     public void chooseMobile()
     {
         storeCommMethod();
         PlayerPrefs.SetString("device", "mobile");
-        panel.transform.LeanMoveLocal(new Vector2(0, -645), 1).setEaseOutQuart();
+        panel.transform.LeanMoveLocal(new Vector2(0, -645), 0.75f).setEaseOutQuart();
     }
     public void chooseTablet()
     {
         storeCommMethod();
         PlayerPrefs.SetString("device", "tablet");
-        panel.transform.LeanMoveLocal(new Vector2(0, -645), 1).setEaseOutQuart();
+        panel.transform.LeanMoveLocal(new Vector2(0, -645), 0.75f).setEaseOutQuart();
+    }
+
+    public void chooseAgain()
+    {
+        panel.transform.LeanMoveLocal(new Vector2(0, 0), 0.75f).setEaseOutQuart();
+        backButton.transform.LeanMoveLocalY(-344, 0.75f).setEaseOutQuart();
+        backButton.transform.LeanRotateZ(90, 0.5f);
     }
 
     private void storeCommMethod()
     {
         PlayerPrefs.SetString("conn_method", "bluetooth");
+        backButton.transform.LeanMoveLocalY(-272, 0.75f).setEaseOutQuart();
+        backButton.transform.LeanRotateZ(-90, 0.5f);
     }
 }
