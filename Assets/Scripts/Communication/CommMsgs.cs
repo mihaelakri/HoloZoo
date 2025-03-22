3ac06bbb7c909880282487d37dd61afc97719c03
Ivan Zelentrović <bluez.du@gmail.com>
2025-03-22 13:38:16 +0100

    fix: model switching when entering learning mode selection

--- Assets/Scripts/Communication/CommMsgs.cs
+++ Assets/Scripts/Communication/CommMsgs.cs
@@ -10,15 +10,25 @@ namespace CommMsgs
         public float x { get; set; }
         public float y { get; set; }
         public float z { get; set; }
-        private int _animal_id;
+        private int _animal_id = -1;
         public int animal_id
         {
             get { return _animal_id; }
             set
             {
+                // Initialization edge-case
+                if (_animal_id == -1)
+                {
+                    _animal_id = value;
+                    return;
+                }
+
                 _animal_id = value;
+
                 if (PlayerPrefs.GetString("device") == "mobile")
+                {
                     BluetoothForAndroid.WriteMessage(MemoryPackSerializer.Serialize(this));
+                }
             }
         }
 
