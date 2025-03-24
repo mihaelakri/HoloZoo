c04b01343fc583fb46ca90a64554049cb930e6ef
Ivan Zelentrović <bluez.du@gmail.com>
2025-03-24 14:23:27 +0100

    fix: continent overlay layout issues

--- Assets/Scripts/ShowContinentAnimals.cs
+++ Assets/Scripts/ShowContinentAnimals.cs
@@ -5,10 +5,11 @@ namespace WPM
 {
     public class ShowContinentAnimals : MonoBehaviour
     {
-        public GameObject animalPanel;
-        public GameObject animalContainer;
-        public GameObject prefabAnimal;
-        public Text continentName;
+        [SerializeField]
+        GameObject animalPanel, animalContainer, prefabAnimal, prefabRow;
+
+        [SerializeField]
+        Text continentName;
         WorldMapGlobe map;
 
         void Start()
@@ -54,9 +55,17 @@ namespace WPM
                 Debug.LogError($"{nameof(ShowContinentAnimals)} - area not found, id_continent: {id_continent}");
             var animals = GameData.Instance.GetAreaAnimals(area, userLevel);
 
+            int rowCounter = 0;
+            GameObject row = Instantiate(prefabRow, animalContainer.transform);
+
             foreach (var animal in animals)
             {
-                var currentAnimal = Instantiate(prefabAnimal, animalContainer.transform);
+                if (rowCounter >= 3)
+                {
+                    row = Instantiate(prefabRow, animalContainer.transform);
+                    rowCounter = 0;
+                }
+                var currentAnimal = Instantiate(prefabAnimal, row.transform);
 
                 // Update panel text and image
                 Text animalNamePrefab = currentAnimal.transform.GetComponentInChildren<Text>();
@@ -69,6 +78,8 @@ namespace WPM
 
                 if (currentAnimal.TryGetComponent<SceneChange>(out var changeSceneScript))
                     changeSceneScript.id = animal.id.ToString();
+
+                rowCounter++;
             }
         }
 
